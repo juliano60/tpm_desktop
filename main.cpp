4ec9ef55510df27707a954df3b05f53e4054d962
Jules Konan <juliano60@hotmail.co.uk>
2016-08-15 07:37:28 +0100

    Added splash screen

--- main.cpp
+++ main.cpp
@@ -1,12 +1,26 @@
 #include "mainwindow.h"
 #include <QApplication>
-#include "createaccount.h"
+#include <QSplashScreen>
+#include <QPixmap>
+#include <QThread>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSplashScreen* splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/splash.jpg"));
+    splash->show();
+
+    Qt::Alignment topRight = Qt::AlignRight | Qt::AlignTop;
+    splash->showMessage(QObject::tr("Loading modules..."), topRight, Qt::white);
+    QThread::sleep(2);
+
     MainWindow w;
-    //CreateAccount w;
+    splash->showMessage(QObject::tr("Establishing connections..."), topRight, Qt::white);
+    QThread::sleep(2);
+
     w.show();
+    splash->finish(&w);
+    delete splash;
     return a.exec();
 }
